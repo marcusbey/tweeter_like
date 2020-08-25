43a553d26e0639094f704025e89e521fd2ae1186
Romain BOBOE <rboboe@gmail.com>
2020-08-25 18:16:58 -0400

    tweet detail view and response handling

--- tweets/views.py
+++ tweets/views.py
@@ -1,12 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Tweet
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse('hello world')
+    return HttpResponse("<h1>Hello World<h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    obj = Tweet.objects.get(id=tweet_id)
-    return HttpResponse(f"Hello Tweet, {args} {obj.content}")
+    """
+    REST API VIRE 
+    consume by JavaScript or Swift / Java
+    return Json
+    """
+    data = {
+        "id": tweet_id,
+        #"image": obj.image.url
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+    return JsonResponse(data, status=status)

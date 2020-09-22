d14dfa79edc99e20c3cac72debd750a7791ef94d
Romain BOBOE <rboboe@gmail.com>
2020-09-22 17:51:10 -0400

    admin model set for filtering

--- tweets/admin.py
+++ tweets/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Tweet
 
-admin.site.register(Tweet)
+
+class TweetAdmin(admin.ModelAdmin):
+    search_fields = ['user__username', 'user__email']
+
+    class Meta:
+        model = Tweet
+
+
+admin.site.register(Tweet, TweetAdmin)
--- tweets/models.py
+++ tweets/models.py
@@ -12,6 +12,9 @@ class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
+    def __str__(self):
+        return self.content[:50]
+
     class Meta:
         ordering = ['-id']
 

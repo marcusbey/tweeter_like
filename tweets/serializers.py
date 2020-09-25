9fba1a3b192aa62915697ca7b2e38bdc33b275bc
Romain BOBOE <rboboe@gmail.com>
2020-09-25 06:55:18 -0400

    tweet serializer

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -38,11 +38,11 @@ class TweetCreateSerializer(serializers.ModelSerializer):
 
 class TweetSerializer(serializers.ModelSerializer):
     likes = serializers.SerializerMethodField(read_only=True)
-    content = serializers.SerializerMethodField(read_only=True)
+    parent = TweetCreateSerializer(read_only=True)
 
     class Meta:
         model = Tweet
-        fields = ['id', 'content', 'likes', 'is_retweet']
+        fields = ['id', 'content', 'likes', 'is_retweet', 'parent']
 
     def get_likes(self, obj):
         return obj.likes.count()

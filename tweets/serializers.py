4787163a0c5c699191dc594eaf7cd27cff81a88f
Romain BOBOE <rboboe@gmail.com>
2020-10-03 16:27:47 -0400

    adding testing for action and twee list

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -41,20 +41,13 @@ class TweetCreateSerializer(serializers.ModelSerializer):
 
 class TweetSerializer(serializers.ModelSerializer):
     likes = serializers.SerializerMethodField(read_only=True)
-    content = serializers.SerializerMethodField(read_only=True)
-    # parent = TweetCreateSerializer(read_only=True)
+    parent = TweetCreateSerializer(read_only=True)
 
     class Meta:
         model = Tweet
         fields = ['id', 'content', 'likes',
-                  'is_retweet']
+                  'is_retweet', 'parent']
         # variable = whatever and source = parent in fields
 
     def get_likes(self, obj):
         return obj.likes.count()
-
-    def get_content(self, obj):
-        content = obj.content
-        if obj.is_retweet:
-            content = obj.parent.content
-        return content

483e764b12f80d136b3aa0d4f61b9667f6178b34
Romain BOBOE <rboboe@gmail.com>
2020-10-07 06:17:03 -0400

    separating function into different components

--- tweetmama-web/src/App.js
+++ tweetmama-web/src/App.js
@@ -1,59 +1,12 @@
-import React, { useEffect, useState} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-
-function loadTweets(callback){
-  const xhr = new XMLHttpRequest();
-  const method = 'GET';
-  const url = "http://localhost:8000/api/tweets"; 
-  const responseType = "json";
-
-  xhr.responseType = responseType;
-  xhr.open(method, url);
-  xhr.onload = function(){
-      callback(xhr.response, xhr.status)
-  }
-  xhr.onerror = function(e) {
-    console.log(e)
-    callback({"message": "The request wa s an error"}, 400)
-  }
-  xhr.send();
-}
-
-
-function ActionBtn(props) {
-  const {tweet, action} = props
-  const className = props.className ? props.className : 'btn btn-primary btn-sm'
-  return action.type === 'like' ? <button className={className}> {tweet.likes} Likes </button> : null
-}
- 
-
-function Tweet(props) {
-  const {tweet} = props
-  const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
-  return <div className={className}>
-    <p>{tweet.id}- {tweet.content}</p>
-    <div className = 'btn btn-group'>
-      <ActionBtn tweet={tweet} action ={{type:"like"}}/>
-      <ActionBtn tweet={tweet} action ={{type:"unlike"}}/>
-      <ActionBtn tweet={tweet} action ={{type:"retweet"}}/>
-    </div>
-  </div>
-}
+import {TweetsList} from './tweets'
 
 
 function App() {
-  const [tweets, setTweets] = useState([{content: '123'}])
-
-  useEffect(() => {
-    const myCallback = (response, status ) => {
-      if (status === 200)
-        setTweets(response) 
-    }
-    loadTweets(myCallback)
-  }, [])
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -62,9 +15,8 @@ function App() {
           Edit <code>src/App.js</code> and same to reload.
         </p>
         <div>
-          {tweets.map((item, index) => {
-            return <Tweet tweet={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`}/>
-            })
+          {
+            < TweetsList />
           }
         </div>
         <a
